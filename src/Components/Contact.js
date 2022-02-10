8d4e707841566e8eed414788d3c36aa75dded566
scottmc500 <smchenry2014@gmail.com>
2022-02-09 22:53:44 -0500

    Adding all of the frontend code

---
+++ src/Components/Contact.js
@@ -0,0 +1,151 @@
+import React, { Component } from 'react';
+import $ from 'jquery';
+
+class Contact extends Component {
+   constructor(props) {
+      super(props);
+      this.state = {
+         contactName: '',
+         contactEmail: '',
+         contactSubject: '',
+         contactMessage: ''
+      };
+      this.handleChange.bind(this);
+      this.handleSubmit.bind(this);
+   }
+
+   handleChange(e) {
+      this.setState({ [e.target.name] : e.target.value });
+   }
+
+   handleSubmit(e) {
+      e.preventDefault();
+
+      $('#image-loader').fadeIn();
+      $('#message-warning').fadeOut();
+
+      const {contactName, contactEmail, contactSubject, contactMessage} = this.state;
+      var data = JSON.stringify({
+         "TableName": "Messages",
+         "Item": {
+            "timestamp": {
+               "S": new Date().toISOString()
+            },
+            "name": {
+               "S": contactName
+            },
+            "email": {
+               "S": contactEmail
+            },
+            "subject": {
+               "S": contactSubject
+            },
+            "message": {
+               "S": contactMessage
+            }
+         }
+      });
+
+      $.ajax({
+         type: "POST",
+         url: process.env.REACT_APP_API_URL,
+         data: data,
+         dataType: "json",
+         headers: {"x-api-key": process.env.REACT_APP_API_KEY}
+      }).done(function(msg) {
+         var str = JSON.stringify(msg);
+         if (str === '{}') {
+            $('#image-loader').fadeOut();
+            $('#message-warning').hide();
+            $('#contactForm').fadeOut();
+            $('#message-success').fadeIn();
+         } else {
+            $('#image-loader').fadeOut();
+            $('#message-warning').html(str);
+            $('#message-warning').fadeIn();
+         }
+      }).fail(function(err) {
+         $('#image-loader').fadeOut();
+         $('#message-warning').html(JSON.stringify(err));
+         $('#message-warning').fadeIn();
+      });
+   }
+   
+   render() {
+      const {data} = this.props;
+
+      if (data) {
+         var name = data.name;
+         var street = data.address.street;
+         var city = data.address.city;
+         var state = data.address.state;
+         var zip = data.address.zip;
+         var phone = data.phone;
+         var message = data.contactmessage;
+      }
+
+      return (
+         <section id="contact">
+            <div className="row section-head">
+            <div className="two columns header-col">
+               <h1><span>Get In Touch.</span></h1>
+            </div>
+            <div className="ten columns">
+                  <p className="lead">{message}</p>
+            </div>
+         </div>
+
+         <div className="row">
+            <div className="eight columns">
+               <form id="contactForm"  name="contactForm" onSubmit={e => this.handleSubmit(e)}>
+					<fieldset>
+                  <div>
+						   <label htmlFor="contactName">Name</label>
+						   <input type="text" defaultValue="" size="35" id="contactName" name="contactName" value={this.state.contactName} onChange={e => this.handleChange(e)} required/>
+                  </div>
+                  <div>
+                     <label htmlFor="contactEmail">Email</label>
+                     <input type="text" defaultValue="" size="35" id="contactEmail" name="contactEmail" value={this.state.contactEmail} onChange={e => this.handleChange(e)} required/>
+                  </div>
+                  <div>
+						   <label htmlFor="contactSubject">Subject</label>
+						   <input type="text" defaultValue="" size="35" id="contactSubject" name="contactSubject" value={this.state.contactSubject} onChange={e => this.handleChange(e)} required/>
+                  </div>
+                  <div>
+                     <label htmlFor="contactMessage">Message</label>
+                     <textarea cols="50" rows="15" id="contactMessage" name="contactMessage" value={this.state.contactMessage} onChange={e => this.handleChange(e)} required/>
+                  </div>
+
+                  <div>
+                     <button className="submit">Submit</button>
+                     <span id="image-loader">
+                        <img alt="" src="images/loader.gif" />
+                     </span>
+                  </div>
+					</fieldset>
+				   </form>
+
+           <div id="message-warning"> Error boy</div>
+				   <div id="message-success">
+                  <i className="fa fa-check"></i>Your message was sent, thank you!<br />
+				   </div>
+           </div>
+
+            <aside className="four columns footer-widgets">
+               <div className="widget widget_contact">
+					   <h4>Address and Phone</h4>
+					   <p className="address">
+						   {name}<br />
+						   {street} <br />
+						   {city}, {state} {zip}<br />
+						   <span>{phone}</span>
+					   </p>
+				   </div>
+            </aside>
+         </div>
+      </section>
+   );
+  }
+}
+
+export default Contact;
---
+++ src/Components/Resume.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+
+class Resume extends Component {
+  render() {
+
+    if(this.props.data){
+      var skillmessage = this.props.data.skillmessage;
+      var education = this.props.data.education.map(function(education){
+        var educationPicture = 'images/education/' + education.picture
+        return <div key={education.school}><img alt={education.school} src={educationPicture} /><h3>{education.school}</h3>
+        <p className="info">{education.degree} <span>&bull;</span><em className="date">{education.graduated}</em></p>
+        <p>{education.description}</p></div>
+      })
+      var work = this.props.data.work.map(function(work){
+        var workPicture = 'images/work/' + work.picture
+        return <div key={work.company}><img alt={work.company} src={workPicture} /><h3>{work.company}</h3>
+            <p className="info">{work.title}<span>&bull;</span> <em className="date">{work.years}</em></p>
+            <p>{work.description}</p>
+        </div>
+      })
+      var skills = this.props.data.skills.map(function(skills){
+        var skillPicture = 'images/tech/'+skills.picture;
+        return <div key={skills.name} className="column">
+                  <img className='skill' alt={skills.name} src={skillPicture} />
+                  <h5>{skills.name}</h5>
+                  <p>{skills.description}</p>
+               </div>
+      })
+    }
+
+    return (
+      <section id="resume">
+
+      <div className="row education">
+         <div className="three columns header-col">
+            <h1><span>Education</span></h1>
+         </div>
+
+         <div className="nine columns main-col">
+            <div className="row item">
+               <div className="twelve columns">
+                 {education}
+               </div>
+            </div>
+         </div>
+      </div>
+
+
+      <div className="row work">
+
+         <div className="three columns header-col">
+            <h1><span>Work</span></h1>
+         </div>
+
+         <div className="nine columns main-col">
+          {work}
+        </div>
+    </div>
+
+
+
+      <div className="row skill">
+
+         <div className="three columns header-col">
+            <h1><span>Skills</span></h1>
+         </div>
+
+        <div className="nine columns main-col">
+          <p>{skillmessage}</p>
+			  </div>
+        
+        <div className="bgrid-quarters s-bgrid-thirds cf">
+          {skills}
+        </div>
+      </div>
+   </section>
+    );
+  }
+}
+
+export default Resume;
