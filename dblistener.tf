b8b84336a19ba9e9ce720e5de506fa20d1974484
scottmc500 <smchenry2014@gmail.com>
2022-02-11 10:38:22 -0500

    Consolidating HCL Code

--- dblistener.tf
+++ dblistener.tf
@@ -1,3 +1,22 @@
+resource "aws_dynamodb_table" "messages-dynamodb-table" {
+  name           = "Messages"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "email"
+  range_key      = "timestamp"
+  stream_enabled   = true
+  stream_view_type = "NEW_AND_OLD_IMAGES"
+
+  attribute {
+    name = "email"
+    type = "S"
+  }
+
+  attribute {
+    name = "timestamp"
+    type = "S"
+  }
+}
+
 resource "aws_sns_topic" "messaging_topic" {
     name = "smm-message-topic"
 }
