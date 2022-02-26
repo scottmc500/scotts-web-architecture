d79730afadef47d8a3c4d994d5a0a461c7cfdeca
scottmc500 <smchenry2014@gmail.com>
2022-02-26 17:31:16 -0500

    Update dblistener.tf

--- terraform/dblistener.tf
+++ terraform/dblistener.tf
@@ -38,11 +38,12 @@ data "archive_file" "lambda_package" {
 }
 
 resource "aws_lambda_function" "message_listener" {
-    filename = "output/lambda.zip"
+    filename = data.archive_file.lambda_package.output_path
     function_name = "db-message-listener"
     role = aws_iam_role.messages_lambda_role.arn
     handler = "lambda_function.lambda_handler"
     runtime = "python3.8"
+    source_code_hash = data.archive_file.lambda_package.output_base64sha256
     environment {
         variables = {
             TOPIC_ARN = aws_sns_topic.messaging_topic.arn
