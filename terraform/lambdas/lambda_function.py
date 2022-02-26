1c084273800d65b3c32b82c5bc1a5d10d99a64c5
scottmc500 <smchenry2014@gmail.com>
2022-02-26 15:23:45 -0500

    Updating and improving lambda function

--- terraform/lambdas/lambda_function.py
+++ terraform/lambdas/lambda_function.py
@@ -4,26 +4,24 @@ from os import environ
 sns_client = boto3.client("sns")
 topic_arn = environ["TOPIC_ARN"]
 
-def lambda_handler(event, context):
-    for record in event['Records']:
-        if record['eventName'] == "INSERT":
-            notify_received(record['dynamodb']['NewImage'])
-        elif record['eventName'] == "REMOVE":
-            notify_deleted(record['dynamodb']['OldImage'])
-    
-def notify_received(data):
+def notify_received(record):
+    data = record['dynamodb']['NewImage']
     subject = "You received a message from {}".format(data['name']['S'])
     message = "Name: {}\nEmail: {}\nSubject: {}\nMessage: {}".format(data['name']['S'],data['email']['S'],data['subject']['S'],data['message']['S'])
-    send_notification(subject,message)
+    sns_client.publish(TopicArn=topic_arn,Subject=subject,Message=message)
     
-def notify_deleted(data):
+def notify_deleted(record):
+    data = record['dynamodb']['OldImage']
     subject = "A message received at {} has been deleted".format(data['timestamp']['S'])
     message = "Timestamp: {}\nName: {}\nEmail: {}\nSubject: {}\nMessage: {}".format(data['timestamp']['S'],data['name']['S'],data['email']['S'],data['subject']['S'],data['message']['S'])
-    send_notification(subject,message)
+    sns_client.publish(TopicArn=topic_arn,Subject=subject,Message=message)
 
-def send_notification(subject,message):
-    sns_client.publish(
-        TopicArn=topic_arn,
-        Message=message,
-        Subject=subject
-    )
+func_map = {
+    "INSERT": notify_received,
+    "REMOVE": notify_deleted
+}
+
+def lambda_handler(event, context):
+    for record in event['Records']:
+        func_map[record['eventName']](record)
+    
