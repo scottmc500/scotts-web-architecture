a0cc042bf27e7f24c94a11f9e5ac3f9ba9403aa4
Scott McHenry <smchenry2014@gmail.com>
2022-08-01 16:06:30 -0400

    Updated s3.tf to make sure it's in line with the latest version of Terraform

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,49 +1,78 @@
 # S3 bucket for website.
 resource "aws_s3_bucket" "www_bucket" {
   bucket = "www.${var.bucket_name}"
-  acl    = "public-read"
   policy = templatefile("templates/s3-policy.json", { bucket = "www.${var.bucket_name}" })
+  tags = var.common_tags
+}
 
+resource "aws_s3_bucket_acl" "www_bucket_acl" {
+  bucket = aws_s3_bucket.www_bucket.id
+  acl = "public-read"
+}
+
+resource "aws_s3_bucket_cors_configuration" "www_bucket_cors" {
+  bucket = aws_s3_bucket.www_bucket.id
   cors_rule {
     allowed_headers = ["Authorization", "Content-Length"]
     allowed_methods = ["GET", "POST"]
     allowed_origins = ["https://www.${var.domain_name}"]
     max_age_seconds = 3000
   }
+}
 
-  website {
-    index_document = "index.html"
-    error_document = "index.html"
+resource "aws_s3_bucket_website_configuration" "www_bucket_website" {
+  bucket = aws_s3_bucket.www_bucket.id
+  index_document {
+    suffix = "index.html"
   }
-
-  logging {
-    target_bucket = aws_s3_bucket.logs_bucket.id
-    target_prefix = "logs/"
+  error_document {
+    key = "index.html"
   }
+}
 
-  tags = var.common_tags
+resource "aws_s3_bucket_logging" "www_bucket_logging" {
+  bucket = aws_s3_bucket.www_bucket.id
+  target_bucket = aws_s3_bucket.logs_bucket.id
+  target_prefix = "logs/"
 }
 
 # S3 bucket for redirecting non-www to www.
 resource "aws_s3_bucket" "root_bucket" {
   bucket = var.bucket_name
-  acl    = "public-read"
   policy = templatefile("templates/s3-policy.json", { bucket = var.bucket_name })
-
-  website {
-    redirect_all_requests_to = "https://www.${var.domain_name}"
-  }
-
   tags = var.common_tags
 }
 
+resource "aws_s3_bucket_acl" "root_bucket_acl" {
+  bucket = aws_s3_bucket.root_bucket.id
+  acl = "public-read"
+}
+
+resource "aws_s3_bucket_website_configuration" "root_bucket_website" {
+  bucket = aws_s3_bucket.root_bucket.id
+  redirect_all_requests_to {
+    host_name = "https://www.${var.domain_name}"
+  }
+}
+
+# S3 bucket for capturing logs
 resource "aws_s3_bucket" "logs_bucket" {
   bucket = "logs.${var.bucket_name}"
-  acl    = "log-delivery-write"
-  lifecycle_rule {
+}
+
+resource "aws_s3_bucket_acl" "logs_bucket_acl" {
+  bucket = aws_s3_bucket.logs_bucket.id
+  acl = "log-delivery-write"
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "logs_bucket_lifecycle" {
+  bucket = aws_s3_bucket.logs_bucket.id
+  rule {
     id      = "logs"
-    prefix  = "logs/"
-    enabled = true
+    filter {
+      prefix  = "logs/"
+    }
+    status = "Enabled"
     expiration {
       days = 90
     }
