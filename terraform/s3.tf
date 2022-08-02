1d600facd7a2035848721d53e4086059bac4f27b
Scott McHenry <smchenry2014@gmail.com>
2022-08-02 11:57:34 -0400

    Update s3.tf

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,8 +1,12 @@
 # S3 bucket for website.
 resource "aws_s3_bucket" "www_bucket" {
   bucket = "www.${var.bucket_name}"
-  policy = templatefile("templates/s3-policy.json", { bucket = "www.${var.bucket_name}" })
   tags = var.common_tags
+}
+
+resource "aws_s3_bucket_policy" "www_bucket_policy" {
+  bucket = aws_s3_bucket.www_bucket.id
+  policy = templatefile("templates/s3-policy.json", { bucket = "www.${var.bucket_name}" })
 }
 
 resource "aws_s3_bucket_acl" "www_bucket_acl" {
@@ -39,8 +43,12 @@ resource "aws_s3_bucket_logging" "www_bucket_logging" {
 # S3 bucket for redirecting non-www to www.
 resource "aws_s3_bucket" "root_bucket" {
   bucket = var.bucket_name
-  policy = templatefile("templates/s3-policy.json", { bucket = var.bucket_name })
   tags = var.common_tags
+}
+
+resource "aws_s3_bucket_policy" "root_bucket_policy" {
+  bucket = aws_s3_bucket.root_bucket.id
+  policy = templatefile("templates/s3-policy.json", { bucket = var.bucket_name })
 }
 
 resource "aws_s3_bucket_acl" "root_bucket_acl" {
@@ -58,6 +66,7 @@ resource "aws_s3_bucket_website_configuration" "root_bucket_website" {
 # S3 bucket for capturing logs
 resource "aws_s3_bucket" "logs_bucket" {
   bucket = "logs.${var.bucket_name}"
+  tags = var.common_tags
 }
 
 resource "aws_s3_bucket_acl" "logs_bucket_acl" {
